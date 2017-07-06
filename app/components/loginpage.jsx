6c662dcbe16e8462a597b45cf2753e219ac4ac0d
Max Zhou <ball908765@gmail.com>
2017-07-05 20:16:54 -0400

    react works now

--- app/components/loginpage.jsx
+++ app/components/loginpage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default class LoginPage extends ReactComponent {
+export default class LoginPage extends React.Component {
     render() {
         return (
             <div className="login">
                 <form className="login_box">
-                    <input type="text" name="login_info" /><br/>
-                    <input type="button" name="submit" >hello </input>
+                    <input type="text" name="login_info"/><br/>
+                    <button type="button" name="submit" >hello </button>
                 </form>
                 <p className="login_info">/*maybe use function to switch b/w username and password*/</p>
             </div>
