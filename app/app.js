a70dfe9dae0219c9b256aedf63327e6fbe863da0
Max Zhou <ball908765@gmail.com>
2017-07-13 02:23:30 -0400

    basic routing

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import Login from './components/layout.jsx';
+import Layout from './components/layout.jsx';
 import LoginPage from './components/loginpage.jsx';
 
-ReactDOM.render(<Login/>, document.getElementById('root'));
+ReactDOM.render(<Layout/>, document.getElementById('root'));
--- app/components/layout.jsx
+++ app/components/layout.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
-import LoginPage from './loginpage.jsx'
+import LoginPage from './loginpage.jsx';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-export default class Login extends React.Component {
+export default class Layout extends React.Component {
     render() {
         return (
-            <div className="app-container">
-                <header>
-                    <nav className = "navbar navbar-default">
-                        Navbar!
-                    </nav>
-                </header>
-                <div className="app-content">
-                    <LoginPage/>
+            <Router>
+                <div className="page-wrapper">
+                    <ul className = "nav nav-pills nav-stacked">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/history">History</Link>
+                        </li>
+                    </ul>
+                    <hr/>
+                    <div className="container-fluid">
+                        <LoginPage/>
+                    </div>
+                    <footer></footer>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/history" component={History}/>
                 </div>
-                <footer></footer>
-            </div>
+            </Router>
         )
     }
 }
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
+
+const History = () => (
+    <div>
+        <h2>Four score and seven years ago...</h2>
+    </div>
+);
